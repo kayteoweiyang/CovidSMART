1022abcd5eb17d3eff504ffa760c3c04f0563ea2
Teo Wei Yang <kayteoweiyang@hotmail.com>
2021-10-17 20:58:17 +0800

    15102021.1

--- app/src/main/java/com/anf/covid_smart/PublicUserHistory.java
+++ app/src/main/java/com/anf/covid_smart/PublicUserHistory.java
@@ -6,16 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PublicUserHistory extends AppCompatActivity {
+
+    TextView et_nric, et_date;
+    ImageView btn_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_user_history);
 
+        et_nric = findViewById(R.id.nricPUH);
+        et_date = findViewById(R.id.datePUH);
+        btn_search = findViewById(R.id.searchPUH);
+        btn_search.setOnClickListener(buttonsOnClickListener);
         BottomNavigationView btmNavView = findViewById(R.id.bottom_navigation);
         btmNavView.setSelectedItemId(R.id.nav_home);
 
@@ -46,4 +59,21 @@ public class PublicUserHistory extends AppCompatActivity {
             }
         });
     }
+    private View.OnClickListener buttonsOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.searchPUH:
+                    if(et_nric.getText() != "") {
+                        Toast.makeText(PublicUserHistory.this, "Searching...", Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(PublicUserHistory.this, "Please key in NRIC", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + v.getId());
+            }
+        }
+    };
 }
