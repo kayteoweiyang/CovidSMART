9f5f68488a4ea264371c2f321e518888fb578fb5
Teo Wei Yang <kayteoweiyang@hotmail.com>
2021-10-17 20:27:40 +0800

    15102021.1

--- app/src/main/java/com/anf/covid_smart/ActiveStatus.java
+++ app/src/main/java/com/anf/covid_smart/ActiveStatus.java
@@ -5,16 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ActiveStatus extends AppCompatActivity {
 
+    TextView sgCasesOrg, sgDeathsOrg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_status);
+
+        sgCasesOrg = findViewById(R.id.sgConfirmOrg);
+        sgDeathsOrg = findViewById(R.id.sgDeathsOrg);
 
         BottomNavigationView btmNavView = findViewById(R.id.bottom_navigation);
         btmNavView.setSelectedItemId(R.id.nav_home);
@@ -45,5 +60,35 @@ public class ActiveStatus extends AppCompatActivity {
                 return false;
             }
         });
+        getSGCases();
+    }
+    private void getSGCases() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObj = new JsonObjectRequest("https://api.covid19api.com/summary", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray countries = response.getJSONArray("Countries");
+                    for (int i = 0; i < countries.length(); i++)
+                    {
+                        JSONObject country = countries.getJSONObject(i);
+                        if(country.getString("Country").equalsIgnoreCase("Singapore")) {
+                            sgCasesOrg.setText(country.getString("TotalConfirmed"));
+                            sgDeathsOrg.setText(country.getString("TotalDeaths"));
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("error", error.toString());
+
+            }
+        });
+        queue.add(jsonObj);
     }
 }
